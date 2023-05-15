096a899832460a1ccd95b0ca2597be7a0b808e58
Jonathan Heathcote <mail@jhnet.co.uk>
2023-05-15 20:58:28 +0100

    Run MTCNN on multiple angles

--- faceie/mtcnn/detect_faces.py
+++ faceie/mtcnn/detect_faces.py
@@ -6,6 +6,8 @@ This submodule implements the overall face detection process
 
 from typing import NamedTuple
 
+from itertools import chain
+
 import numpy as np
 from numpy.typing import NDArray
 
@@ -36,64 +38,80 @@ def resolve_p_net_bounding_boxes(bounding_boxes: NDArray) -> NDArray:
     return bounding_boxes
 
 
-def make_square(bounding_boxes: NDArray, axis: int) -> NDArray:
+def bounding_boxes_to_centers(bounding_boxes: NDArray) -> NDArray:
+    """
+    Given an array of bounding boxes, return their center coordinates.
+
+    Parameters
+    ==========
+    bounding_boxes : array (..., 4)
+
+    Returns
+    =======
+    array (..., 2)
+    """
+    return np.sum(bounding_boxes.reshape(-1, 2, 2), axis=-2) / 2.0
+
+
+def bounding_boxes_to_longest_sides(bounding_boxes: NDArray) -> NDArray:
+    """
+    Given an array of bounding boxes, return their longest sides.
+
+    Parameters
+    ==========
+    bounding_boxes : array (..., 4)
+
+    Returns
+    =======
+    array (...)
+    """
+    lengths = np.diff(bounding_boxes.reshape(-1, 2, 2), axis=-2).reshape(-1, 2)
+    return np.max(lengths, axis=-1)
+
+
+def centers_to_square_bounding_boxes(centres: NDArray, sizes: NDArray) -> NDArray:
+    """
+    Given an array of suqare bounding box centers and their sizes, returns an
+    array of bounding boxes.
+
+    Parameters
+    ==========
+    centers: array (..., 2)
+    sizes: array (...)
+
+    Returns
+    =======
+    array (..., 4)
+    """
+    half_sizes = np.expand_dims(sizes / 2.0, -1)
+    return np.concatenate(
+        [
+            centres - half_sizes,
+            centres + half_sizes,
+        ],
+        axis=-1,
+    )
+
+
+def make_square(bounding_boxes: NDArray) -> NDArray:
     """
     Given an array of bounding boxes, enlarge these as necessary, centered on
     the existing centre point, to make them square.
 
     Parameters
     ==========
-    bounding_boxes: array (..., 4, ...)
+    bounding_boxes: array (..., 4)
         An array which contains a 4-long dimension enumerating the x1, y1, x2
         and y2 coordinates of a bounding box.
-    axis : int
-        The index of the axis enumerating the bounding box coordinates.
 
     Returns
     =======
-    array (same shape as bounding_boxes)
+    array (..., 4)
         A new bounding box array where all bounding boxes have x2-x1 == y2-y1.
     """
-    # Make axis index positive
-    axis %= bounding_boxes.ndim
-
-    # Sanity check
-    assert bounding_boxes.shape[axis] == 4
-
-    # This function returns an index like (..., n, ...) with the 'n' on the
-    # specified axis.
-    I = lambda n: tuple(
-        n if i == axis else slice(None) for i in range(bounding_boxes.ndim)
-    )
-    IX1 = I(0)
-    IY1 = I(1)
-    IX2 = I(2)
-    IY2 = I(3)
-
-    x1s = bounding_boxes[IX1]
-    y1s = bounding_boxes[IY1]
-    x2s = bounding_boxes[IX2]
-    y2s = bounding_boxes[IY2]
-
-    # Find longest of width and height
-    ws = x2s - x1s
-    hs = y2s - y1s
-    ls = np.maximum(ws, hs)
-
-    # Compute centre of bounding boxes
-    cxs = x1s + (ws * 0.5)
-    cys = y1s + (hs * 0.5)
-
-    half_ls = ls * 0.5
-
-    # Compute new bounding boxes
-    out = np.empty_like(bounding_boxes)
-    out[IX1] = cxs - half_ls
-    out[IY1] = cys - half_ls
-    out[IX2] = cxs + half_ls
-    out[IY2] = cys + half_ls
-
-    return out
+    centers = bounding_boxes_to_centers(bounding_boxes)
+    longest_sides = bounding_boxes_to_longest_sides(bounding_boxes)
+    return centers_to_square_bounding_boxes(centers, longest_sides)
 
 
 def resolve_coordinates(
@@ -204,7 +222,7 @@ def refine_proposals(
 ) -> tuple[NDArray, NDArray]:
     """
     Given a series of potential faces, return a more accurate probability and
-    bounding box.
+    bounding box using the :py:func:`r_net`.
 
     Parameters
     ==========
@@ -225,7 +243,7 @@ def refine_proposals(
         (x1, y1, x2, y2) in image pixel coordinates. The x2 and y2 coordinates
         are inclusive.
     """
-    crops = make_square(bounding_boxes, axis=1)
+    crops = make_square(bounding_boxes)
 
     # Crop candidate faces from inputs
     faces = np.empty((crops.shape[0], 3, 24, 24), dtype=np.float32)
@@ -252,7 +270,8 @@ def output_proposals(
 ) -> tuple[NDArray, NDArray, NDArray]:
     """
     Given a series of potential faces, return a final (output) probability,
-    bounding box and (optional) set of facial landmarks.
+    bounding box and (optional) set of facial landmarks using the
+    :py:func:`o_net`.
 
     Parameters
     ==========
@@ -277,7 +296,7 @@ def output_proposals(
         gives (x1, y1, x2, y2, ...) giving the coordinates of the left eye,
         right eye, nose, left mouth and right mouth.
     """
-    crops = make_square(bounding_boxes, axis=1)
+    crops = make_square(bounding_boxes)
 
     # Crop candidate faces from inputs
     faces = np.empty((crops.shape[0], 3, 48, 48), dtype=np.float32)
@@ -299,41 +318,8 @@ def output_proposals(
     return probs, bboxes, landmarks
 
 
-class DetectedFaces(NamedTuple):
-    """
-    The output of :py:func:`detect_faces`.
-    """
-
-    probabilities: NDArray
-    """
-    A (num_faces, ) shaped array giving the probability score (0.0 - 1.0)
-    assigned to each detected face.
-    """
-
-    bounding_boxes: NDArray
-    """
-    A (num_faces, 4) shaped array giving the coordinates of a bounding box for
-    each detected face as x1, y1, x2 and y2. Coordinates are given in terms of
-    input pixels and are *inclusive*.
-    """
-
-    landmarks: NDArray
-    """
-    A (num_faces, 10) shaped array giving the coordinates of five detected facial
-    landmarks in each face. These are given as a series of pixel coordinates
-    (x1, y1, x2, y2, ...) with the landmarks being:
-    
-    * Left eye
-    * Right eye
-    * Nose
-    * Left of mouth
-    * Right of mouth
-    """
-
-
-def detect_faces(
-    image: Image.Image,
-    pyramid_downscale_factor: float = np.sqrt(2),
+def detect_faces_single_orientation(
+    pyramid: ImagePyramid,
     proposal_skip_downscale_factor: float = 4.0,
     proposal_probability_threshold: float = 0.7,
     refine_probability_threshold: float = 0.7,
@@ -341,17 +327,14 @@ def detect_faces(
     proposal_maximum_iou: float = 0.5,
     refine_maximum_iou: float = 0.5,
     output_maximum_iou: float = 0.5,
-) -> DetectedFaces:
+) -> tuple[NDArray, NDArray, NDArray]:
     """
     Detect faces within an image using the MTCNN algorithm by Zhang et al.
 
     Parameters
     ==========
-    image : Image
-        The image to detect faces within.
-    pyramid_downscale_factor : float
-        The image will be processed at a range of progressively sizes, related
-        by this factor.
+    pyramid : ImagePyramid
+        An Image Pyramid with the image to detect faces within.
     proposal_skip_downscale_factor : float
         Skips running the proposal filtering process on images below this
         scaling factor. The choice of '4' as a default prevents the use of
@@ -370,14 +353,18 @@ def detect_faces(
 
     Returns
     =======
-    DetectedFaces
-        For each detected face, a probability, bounding box and set of facial
-        landmarks.
+    probabilities : array (num_accepted_proposals)
+        The (output) probabilities of the input faces which surpass the given
+        probability_threshold.
+    bounding_boxes : array (num_accepted_proposals, 4)
+        For each accepted face, a new refined bounding box.  Dimension 1 gives
+        (x1, y1, x2, y2) in image pixel coordinates. The x2 and y2 coordinates
+        are inclusive.
+    landmarks : array (num_accepted_proposals, 10)
+        For each accepted face, the facial landmark coordinates. Dimension 1
+        gives (x1, y1, x2, y2, ...) giving the coordinates of the left eye,
+        right eye, nose, left mouth and right mouth.
     """
-    pyramid = ImagePyramid(
-        image, min_size=12, downscale_factor=pyramid_downscale_factor
-    )
-
     # First 'proposal' stage. Run convolution at multiple scales
     # ----------------------------------------------------------
     all_level_probs = []
@@ -385,7 +372,7 @@ def detect_faces(
 
     first_level = pyramid.closest_level(proposal_skip_downscale_factor)
     for level, image in enumerate(pyramid):
-        if level < first_level:
+        if level < first_level or image.size[0] < 12 or image.size[1] < 12:
             continue
 
         probs, bboxes = get_proposals(
@@ -430,4 +417,296 @@ def detect_faces(
     bboxes = bboxes[selection]
     landmarks = landmarks[selection]
 
-    return DetectedFaces(probs, bboxes, landmarks)
+    return probs, bboxes, landmarks
+
+
+class DetectedFaces(NamedTuple):
+    """
+    The output of :py:func:`detect_faces`.
+    """
+
+    probabilities: NDArray
+    """
+    A (num_faces, ) shaped array giving the probability score (0.0 - 1.0)
+    assigned to each detected face.
+    """
+
+    angles: NDArray
+    """
+    A (num_faces, ) shaped array giving the angle at which the image was
+    rotated (in radians) when a given face was detected.
+    """
+
+    bounding_boxes: NDArray
+    """
+    A (num_faces, 4) shaped array giving the coordinates of a bounding box for
+    each detected face as x1, y1, x2 and y2. Coordinates are given in terms of
+    input pixels and are *inclusive*.
+    
+    Note that the coordinates given are the un-rotated coordinates (i.e. those
+    in the un-rotated input image). When angle != 0, you will need to rotate
+    the image (and these coordinates) to extract a face accordingly.
+    """
+
+    landmarks: NDArray
+    """
+    A (num_faces, 10) shaped array giving the coordinates of five detected facial
+    landmarks in each face. These are given as a series of pixel coordinates
+    (x1, y1, x2, y2, ...) with the landmarks being:
+    
+    * Left eye
+    * Right eye
+    * Nose
+    * Left of mouth
+    * Right of mouth
+    """
+
+    images: list[Image.Image] | None
+    """
+    If requested, includes a cropped image of each face (rotated to the angle
+    at which it was recognised by the model).
+    
+    All images are square-cropped by expanding the bounding box area into a
+    square. To obtain just the bounding-box area, you will need to further crop
+    these images according to the aspect ratio of the corresponding bounding
+    box.
+    """
+
+
+def translate(coords: NDArray, dx: float, dy: float) -> NDArray:
+    """
+    Apply a translation to a set of coordinates.
+
+    Parameters
+    ==========
+    coords : array (..., even-number-of-coords)
+        The Input coordintaes in the final dimension (where even numbered
+        entries are x-coordinates and odd numbered entries are y-coordinates.
+    dx : float
+    dy : float
+        The translation to be applied
+    """
+    out = coords.copy()
+    out.T[0::2] += dx
+    out.T[1::2] += dy
+    return out
+
+
+def rotate(coords: NDArray, angle: float) -> NDArray:
+    """
+    Rotate a collection of coordinates around the origin, counter-clockwise.
+
+    Parameters
+    ==========
+    coords : array (..., even-number-of-coords)
+        The Input coordintaes in the final dimension (where even numbered
+        entries are x-coordinates and odd numbered entries are y-coordinates.
+    angle : float
+        Angle (in radians) to rotate the coordinates by.
+    """
+    rotation = np.array(
+        [
+            [np.cos(angle), -np.sin(angle)],
+            [np.sin(angle), np.cos(angle)],
+        ]
+    )
+
+    # (..., 2N) -> (..., N, 2)
+    coords = coords.reshape(*coords.shape[:-1], coords.shape[-1] // 2, 2)
+    # (..., N, 2) -> (..., 2, N)
+    coords = np.moveaxis(coords, -1, -2)
+
+    # Perform the actual rotation
+    out = rotation @ coords  # (..., 2, N)
+
+    # (..., 2, N) -> (..., N, 2)
+    out = np.moveaxis(out, -1, -2)
+    # (..., N, 2) -> (..., 2N)
+    out = out.reshape(*out.shape[:-2], out.shape[-2] * 2)
+
+    return out
+
+
+def detect_faces(
+    image: Image.Image,
+    pyramid_downscale_factor: float = np.sqrt(2),
+    rotations: NDArray = np.array([-np.pi / 2, -np.pi / 4, 0.0, np.pi / 4, np.pi / 2]),
+    proposal_skip_downscale_factor: float = 4.0,
+    proposal_probability_threshold: float = 0.7,
+    refine_probability_threshold: float = 0.7,
+    output_probability_threshold: float = 0.7,
+    proposal_maximum_iou: float = 0.5,
+    refine_maximum_iou: float = 0.5,
+    output_maximum_iou: float = 0.5,
+    rotation_maximum_iou: float = 0.25,
+    extract_images: int | None = 160,
+) -> DetectedFaces:
+    """
+    Detect faces within an image using the MTCNN algorithm by Zhang et al.
+
+    Parameters
+    ==========
+    image : Image
+        The image to detect faces within.
+    pyramid_downscale_factor : float
+        The image will be processed at a range of progressively sizes, related
+        by this factor.
+    rotations : [radians, ...]
+        The angles (in radians) at which to rotate the input image when searching.
+    proposal_skip_downscale_factor : float
+        Skips running the proposal filtering process on images below this
+        scaling factor. The choice of '4' as a default prevents the use of
+        up-scaled faces in the refinement and output stages (whose input sizes
+        are twice and four times that of the proposal stage).
+    proposal_probability_threshold : float
+    refine_probability_threshold : float
+    output_probability_threshold : float
+        The is-face probability threshold for a face to be accepted during each
+        of the three filtering stages.
+    proposal_maximum_iou : float
+    refine_maximum_iou : float
+    output_maximum_iou : float
+    rotation_maximum_iou : float
+        The threshold of the intersection-over-union above which two rectangles
+        are duplicates during each of the three processing stages and the final
+        combining of rotated detections.
+    extract_images : int or None
+        If a number of pixels is given, includes a
+        extract_images-by-extract_images pixel square-crop of each detected
+        face in the output. If None, face crops are not extracted.
+
+        The default size of 160x160 pixels is used by the FaceNet-PyTorch
+        implementation of the FaceNet model. This differs from the original
+        FaceNet/Inception-ResNet-v1 paper which is shown using an input size of
+        299x299 pixels. We use the former size by default to better match
+        FaceNet-PyTorch from which we're re-using the weights.
+
+    Returns
+    =======
+    DetectedFaces
+        For each detected face, a probability, angle, bounding box, set of
+        facial landmarks and (optionally) a cropped image.
+    """
+    all_pyramids = []
+    all_probs = []
+    all_angles = []
+    all_bboxes = []
+    all_bboxes_normalised = (
+        []
+    )  # Internal use: Corners of a square centered on the detected face
+    all_bboxes_rotated = []  # Internal use: Bounding box in rotated image coordinates
+    all_landmarks = []
+
+    # Run face detection on all rotations
+    for angle_rad in rotations:
+        angle_deg = (angle_rad / np.pi) * 180
+        if angle_rad != 0:
+            rotated_image = image.rotate(
+                angle_deg, resample=Image.Resampling.BICUBIC, expand=True
+            )
+        else:
+            rotated_image = image
+
+        pyramid = ImagePyramid(
+            rotated_image,
+            min_size=12,
+            downscale_factor=pyramid_downscale_factor,
+        )
+
+        probs, bboxes, landmarks = detect_faces_single_orientation(
+            pyramid=pyramid,
+            proposal_skip_downscale_factor=proposal_skip_downscale_factor,
+            proposal_probability_threshold=proposal_probability_threshold,
+            refine_probability_threshold=refine_probability_threshold,
+            output_probability_threshold=output_probability_threshold,
+            proposal_maximum_iou=proposal_maximum_iou,
+            refine_maximum_iou=refine_maximum_iou,
+            output_maximum_iou=output_maximum_iou,
+        )
+
+        # NB: bounding-box and landmark coordinates rotated into original image
+        # coordinates.
+        all_pyramids.append([pyramid] * len(probs))
+        all_angles.append(np.full(probs.shape, angle_rad))
+        all_probs.append(probs)
+        all_bboxes.append(
+            translate(
+                rotate(
+                    translate(
+                        bboxes,
+                        -rotated_image.size[0] / 2,
+                        -rotated_image.size[1] / 2,
+                    ),
+                    angle_rad,
+                ),
+                image.size[0] / 2,
+                image.size[1] / 2,
+            )
+        )
+        all_landmarks.append(
+            translate(
+                rotate(
+                    translate(
+                        landmarks,
+                        -rotated_image.size[0] / 2,
+                        -rotated_image.size[1] / 2,
+                    ),
+                    angle_rad,
+                ),
+                image.size[0] / 2,
+                image.size[1] / 2,
+            )
+        )
+
+        # For the purposes of later NMS, we normalise the detected bounding
+        # boxes to squares oriented at 0 degrees centered on their original
+        # location. This isn't ideal but it is easy!
+        longest_sides = bounding_boxes_to_longest_sides(bboxes)
+        centers = translate(
+            rotate(
+                translate(
+                    bounding_boxes_to_centers(bboxes),
+                    -rotated_image.size[0] / 2,
+                    -rotated_image.size[1] / 2,
+                ),
+                angle_rad,
+            ),
+            image.size[0] / 2,
+            image.size[1] / 2,
+        )
+        all_bboxes_normalised.append(
+            centers_to_square_bounding_boxes(centers, longest_sides)
+        )
+
+        # Bounding-box coordinates in rotated image domain (used to extract
+        # face crops)
+        all_bboxes_rotated.append(bboxes)
+
+    # Combine results
+    pyramids = list(chain(*all_pyramids))
+    probs = np.concatenate(all_probs)
+    angles = np.concatenate(all_angles)
+    bboxes = np.concatenate(all_bboxes)
+    bboxes_normalised = np.concatenate(all_bboxes_normalised)
+    bboxes_rotated = np.concatenate(all_bboxes_rotated)
+    landmarks = np.concatenate(all_landmarks)
+
+    # Remove duplicates
+    selection = non_maximum_suppression(probs, bboxes_normalised, rotation_maximum_iou)
+
+    pyramids = [pyramids[i] for i in selection]
+    probs = probs[selection]
+    angles = angles[selection]
+    bboxes = bboxes[selection]
+    bboxes_rotated = bboxes_rotated[selection]
+    landmarks = landmarks[selection]
+
+    # Extract face images from input
+    images: list[Image.Image] | None = None
+    if extract_images is not None:
+        images = [
+            pyramid.extract((x1, y1, x2, y2), (extract_images, extract_images))
+            for pyramid, (x1, y1, x2, y2) in zip(pyramids, make_square(bboxes_rotated))
+        ]
+
+    return DetectedFaces(probs, angles, bboxes, landmarks, images)
--- tests/mtcnn/test_mtcnn.py
+++ tests/mtcnn/test_mtcnn.py
@@ -7,18 +7,75 @@ from pathlib import Path
 import numpy as np
 
 from faceie.mtcnn.detect_faces import (
+    bounding_boxes_to_centers,
+    bounding_boxes_to_longest_sides,
+    centers_to_square_bounding_boxes,
     make_square,
     ImagePyramid,
     DetectedFaces,
+    detect_faces_single_orientation,
     detect_faces,
+    translate,
+    rotate,
 )
 
 
 TEST_IMAGE_DIR = Path(__file__).parent
 
 
-@pytest.mark.parametrize("axis", [2, -1])
-def test_make_square(axis: int) -> None:
+def test_bounding_boxes_to_centers() -> None:
+    ar = np.array(
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+        ]
+    )
+
+    assert np.array_equal(
+        bounding_boxes_to_centers(ar),
+        np.array(
+            [
+                [2, 3],
+                [6, 7],
+            ]
+        ),
+    )
+
+
+def test_bounding_boxes_to_longest_sides() -> None:
+    ar = np.array(
+        [
+            [1, 2, 4, 4],
+            [5, 6, 8, 10],
+        ]
+    )
+
+    assert np.array_equal(
+        bounding_boxes_to_longest_sides(ar),
+        np.array([3, 4]),
+    )
+
+
+def test_centers_to_square_bounding_boxes() -> None:
+    ar = np.array(
+        [
+            [2, 3],
+            [6, 7],
+        ]
+    )
+
+    assert np.array_equal(
+        centers_to_square_bounding_boxes(ar, 2),
+        np.array(
+            [
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+            ]
+        ),
+    )
+
+
+def test_make_square() -> None:
     input = np.array(
         [
             [
@@ -26,7 +83,7 @@ def test_make_square(axis: int) -> None:
                 [10, 10, 20, 16],  # Wide
                 [10, 10, 16, 20],  # Tall
             ]
-        ]
+        ],
     )
 
     exp = np.array(
@@ -36,20 +93,62 @@ def test_make_square(axis: int) -> None:
                 [10, 8, 20, 18],
                 [8, 10, 18, 20],
             ]
+        ],
+    )
+
+    out = make_square(input)
+
+    assert np.allclose(out, exp)
+
+
+def test_translate() -> None:
+    ar = np.array(
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16],
         ]
     )
 
-    out = make_square(input, axis)
+    assert np.array_equal(
+        translate(ar, 100, 1000),
+        np.array(
+            [
+                [101, 1002, 103, 1004],
+                [105, 1006, 107, 1008],
+                [109, 1010, 111, 1012],
+                [113, 1014, 115, 1016],
+            ]
+        ),
+    )
 
-    assert np.array_equal(out, exp)
-    assert out.dtype is input.dtype
+
+def test_rotate() -> None:
+    ar = np.array(
+        [
+            [2, 0, 0, 2],
+            [np.sqrt(2), np.sqrt(2), np.sqrt(2), -np.sqrt(2)],
+        ]
+    )
+
+    assert np.allclose(
+        rotate(ar, np.pi / 4),
+        np.array(
+            [
+                [np.sqrt(2), np.sqrt(2), -np.sqrt(2), np.sqrt(2)],
+                [0, 2, 2, 0],
+            ]
+        ),
+    )
 
 
-def test_detect_faces() -> None:
+def test_detect_faces_single_orientation() -> None:
     # Just sanity check that it finds the two faces in this image (cropped
     # such that they are at approximatley the right place.
     im = Image.open(TEST_IMAGE_DIR / "two_faces.jpg")
-    probs, bboxes, landmarks = detect_faces(im)
+    pyramid = ImagePyramid(im, 1)
+    probs, bboxes, landmarks = detect_faces_single_orientation(pyramid)
 
     # Should have found two faces
     assert len(probs) == 2
@@ -69,3 +168,50 @@ def test_detect_faces() -> None:
 
     assert ly == pytest.approx(2 / 5, abs=0.1)
     assert ry == pytest.approx(2 / 5, abs=0.1)
+
+
+@pytest.mark.parametrize("extract_images", [160, None])
+def test_detect_faces_multiple_orientations(extract_images: int | None) -> None:
+    # Just sanity check that it finds the two (differently oriented) faces in this image
+    im = Image.open(TEST_IMAGE_DIR / "faces_at_angles.jpg")
+    out = detect_faces(
+        im,
+        rotations=[-np.pi / 2, -np.pi / 4, 0.0, np.pi / 4, np.pi / 2],
+        extract_images=extract_images,
+    )
+
+    # Should have found two faces
+    assert len(out.probabilities) == 2
+
+    # Should have found one upright face and one leaning to the right
+    assert set(out.angles) == {0.0, np.pi / 4}
+
+    leaning_face = np.argmax(out.angles)
+    upright_face = np.argmin(out.angles)
+
+    # The leaning face should be on the bottom-left third
+    centers = bounding_boxes_to_centers(out.bounding_boxes)
+    centers[..., 0] /= im.size[0]
+    centers[..., 1] /= im.size[1]
+    assert np.allclose(centers[leaning_face], [0.33, 0.66], atol=0.1)
+    assert np.allclose(centers[upright_face], [0.66, 0.33], atol=0.1)
+
+    # Landmarks should be within their bounding boxes (i.e. check at least the
+    # rotations are consistent
+    for angle, bbox, landmarks in zip(out.angles, out.bounding_boxes, out.landmarks):
+        bbox = rotate(bbox, -angle)
+        landmarks = rotate(landmarks, -angle)
+
+        assert bbox[0] < bbox[2]
+        assert bbox[1] < bbox[3]
+
+        for x, y in landmarks.reshape(-1, 2):
+            assert bbox[0] < x < bbox[2]
+            assert bbox[1] < y < bbox[3]
+
+    # Extracted images should be of right size (or absent, as required)
+    if extract_images is None:
+        assert out.images is None
+    else:
+        for image in out.images:
+            assert image.size == (extract_images, extract_images)
